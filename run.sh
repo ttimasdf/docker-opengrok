4964e1583555cf8798935911dc68eac368134ed4
ttimasdf <ttimasdf@users.noreply.github.com>
2017-05-17 00:14:17 +0800

    Swap foreground and background procs

--- run.sh
+++ run.sh
@@ -4,12 +4,6 @@ export JAVA_OPTS="-Xmx8192m -server"
 export OPENGROK_FLUSH_RAM_BUFFER_SIZE="-m 256"
 sysctl -w fs.inotify.max_user_watches=8192000
 
-echo "==================== Waiting for Tomcat ===================="
-catalina.sh run &
-echo "Waiting for service become available... [this may take long]"
-wget -q --tries inf --retry-connrefused http://127.0.0.1:8080
-
-
 if [ -n "$FORCE_REINDEX_ON_BOOT" -o ! -e $OPENGROK_INSTANCE_BASE/data/timestamp ]; then
   echo "================ Running first-time indexing ================"
   cd /opengrok/bin
@@ -20,15 +14,27 @@ fi
 echo "================ Waiting for source updates... ================"
 touch $OPENGROK_INSTANCE_BASE/reindex
 
-if [ $INOTIFY_NOT_RECURSIVE ]; then
-  INOTIFY_CMDLINE="inotifywait -m -e CLOSE_WRITE $OPENGROK_INSTANCE_BASE/reindex"
-else
-  INOTIFY_CMDLINE="inotifywait -mr -e CLOSE_WRITE /src"
-fi
+function file_watch {
+  if [ $INOTIFY_NOT_RECURSIVE ]; then
+    INOTIFY_CMDLINE="inotifywait -m -e CLOSE_WRITE $OPENGROK_INSTANCE_BASE/reindex"
+  else
+    INOTIFY_CMDLINE="inotifywait -mr -e CLOSE_WRITE /src"
+  fi
 
-$INOTIFY_CMDLINE | while read f; do
-  printf "*** %s\n" "$f"
-  echo "*** Updating index"
-  cd /opengrok/bin
-  ./OpenGrok index /src
-done
+  $INOTIFY_CMDLINE | while read f; do
+    echo "================ Updating index ================"
+    printf "...Due to file changed %s\n" "$f"
+    cd /opengrok/bin
+    ./OpenGrok index /src
+  done
+}
+
+file_watch &
+
+echo "==================== Waiting for Tomcat ===================="
+echo "Waiting for service become available... [this may take long]"
+{
+  wget -nv --tries inf --retry-connrefused http://127.0.0.1:8080 -O /dev/null &&
+  echo "==================== Bootstrap Completed ===================="
+} &
+catalina.sh run
