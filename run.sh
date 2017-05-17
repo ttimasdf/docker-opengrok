6fef6cbecc9876879e70600543cea26e4de0e55b
ttimasdf <ttimasdf@users.noreply.github.com>
2017-05-17 09:18:26 +0800

    Another method to make index proc silent

--- run.sh
+++ run.sh
@@ -6,9 +6,9 @@ sysctl -w fs.inotify.max_user_watches=8192000
 
 if [ -n "$FORCE_REINDEX_ON_BOOT" -o ! -e $OPENGROK_INSTANCE_BASE/data/timestamp ]; then
   echo "================ Running first-time indexing ================"
-  cd /opengrok/bin
-  ./OpenGrok index /src
+  OPENGROK_WEBAPP_CFGADDR=none /opengrok/bin/OpenGrok index /src
 fi
+
 
 # ... and we keep running the indexer to keep the container on
 echo "================ Waiting for source updates... ================"
